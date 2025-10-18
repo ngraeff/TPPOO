6b4cccab1d20f9020c7bdf15513d2ffba6f1fd06
Nicolas Graeff <nicolasgraeff02@gmail.com>
2025-10-18 03:24:53 -0300

    desarrollo dificultad

--- src/main/java/org/example/controlador/ControladorJuego.java
+++ src/main/java/org/example/controlador/ControladorJuego.java
@@ -1,5 +1,23 @@
 package org.example.controlador;
 
+import org.example.enums.Dificultad;
+import org.example.enums.EstadoDeJuego;
+import org.example.modelo.Jugador;
+import org.example.modelo.Partida;
+import org.example.modelo.Ranking;
+
 public class ControladorJuego {
+    private Partida partida;
+    private Ranking ranking;
+    private Jugador jugador;
+
+    public ControladorJuego() {
+
+    }
+    //Dificultad dificultad, EstadoDeJuego estadoDeJuego, int estadoJugador, int creditosJugador, int puntosJugador, int vidaJugador
+    public void iniciarPartida(Dificultad dificultad,int estadoJugador, int creditosJugador){
+        this.partida = new Partida(dificultad, EstadoDeJuego.INICIANDO,estadoJugador,creditosJugador, dificultad.getPuntosInicialJugador(),dificultad.getVidaInicialJugador());
+    }
+
 
 }
--- src/main/java/org/example/enums/Dificultad.java
+++ src/main/java/org/example/enums/Dificultad.java
@@ -1,7 +1,27 @@
 package org.example.enums;
 
 public enum Dificultad {
-    FACIL,
-    MEDIO,
-    DIFICIL
+    FACIL(5,0,1),
+    MEDIO(3,0,2),
+    DIFICIL(1,0,3);
+
+    private final int vidaInicialJugador;
+    private final int puntosInicialJugador;
+    private final int velocidadNavesEnemigas;
+
+    Dificultad(int vidaInicialJugador, int puntosInicialJugador, int velocidadNavesEnemigas) {
+        this.vidaInicialJugador = vidaInicialJugador;
+        this.puntosInicialJugador = puntosInicialJugador;
+        this.velocidadNavesEnemigas = velocidadNavesEnemigas;
+    }
+
+    public int getVidaInicialJugador() {
+        return vidaInicialJugador;
+    }
+    public int getPuntosInicialJugador() {
+        return puntosInicialJugador;
+    }
+    public int getVelocidadNavesEnemigas() {
+        return velocidadNavesEnemigas;
+    }
 }
