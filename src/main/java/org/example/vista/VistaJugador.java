03cdc8cb895f1308a76f0075f1cc7d83c29d7e66
Nicolas Graeff <nicolasgraeff02@gmail.com>
2025-10-25 17:29:49 -0300

    imagenes nave y jugador

--- src/main/java/org/example/vista/VistaJugador.java
+++ src/main/java/org/example/vista/VistaJugador.java
@@ -1,29 +1,24 @@
 package org.example.vista;
 import org.example.modelo.Jugador;
+
+import javax.swing.*;
 import java.awt.*;
 import java.util.List;
 
 public class VistaJugador {
 
-    private Color colorJugador = Color.GREEN;
+    private Image imagenJugador;
+
+    public VistaJugador() {
+        imagenJugador = new ImageIcon(getClass().getResource("/img/jugador.png")).getImage();
+    }
 
     public void dibujar(Graphics2D g2d, List<Integer> datosJugador) {
         if (datosJugador == null) return;
-        int posicionX = datosJugador.get(0);
-        int posicionY = datosJugador.get(1);
+        int x = datosJugador.get(0);
+        int y = datosJugador.get(1);
         int ancho = datosJugador.get(2);
         int alto = datosJugador.get(3);
-        g2d.setColor(colorJugador);
-        g2d.fillRect(
-                posicionX,
-                posicionY,
-                ancho,
-                alto
-        );
-    }
-
-    // Opcional: permitir cambiar color según estado
-    public void setColorJugador(Color colorJugador) {
-        this.colorJugador = colorJugador;
+        g2d.drawImage(imagenJugador, x, y, ancho, alto, null);
     }
 }
--- src/main/java/org/example/vista/VistaNaveInvasora.java
+++ src/main/java/org/example/vista/VistaNaveInvasora.java
@@ -1,21 +1,27 @@
 package org.example.vista;
 
-import org.example.modelo.NaveInvasora;
+import javax.swing.*;
 import java.awt.*;
 import java.util.List;
 
 public class VistaNaveInvasora {
 
+    private Image imagenNave;
     private Color colorNaves = Color.RED;
     private final int anchoNave = 30;
     private final int altoNave = 20;
 
+    public VistaNaveInvasora() {
+        imagenNave = new ImageIcon(getClass().getResource("/img/naveInvasora.png")).getImage();
+    }
+
     public void dibujar(Graphics2D g2d, List<int[]> datosNaves) {
-        g2d.setColor(Color.RED);
+
+
         for (int[] datos : datosNaves) {
             int x = datos[0];
             int y = datos[1];
-            g2d.fillRect(x, y, anchoNave, altoNave);
+            g2d.drawImage(imagenNave,x, y, anchoNave, altoNave,null);
         }
     }
 
